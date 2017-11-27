3dfbc5ead54f43ae525621a83fba54e86c0cbffb
yajun.duan <yajun.duan@zymobi.com>
2017-11-27 14:36:04 +0800

    修正屏幕尺寸获取方式

--- src/org/zywx/wbpalmstar/plugin/uexgestureunlock/util/GestureUtil.java
+++ src/org/zywx/wbpalmstar/plugin/uexgestureunlock/util/GestureUtil.java
@@ -1,14 +1,17 @@
 package org.zywx.wbpalmstar.plugin.uexgestureunlock.util;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.view.WindowManager;
 
 public class GestureUtil {
 
     public static int[] getScreenDisplay(Context context) {
         WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        int width = windowManager.getDefaultDisplay().getWidth();// 手机屏幕的宽度
-        int height = windowManager.getDefaultDisplay().getHeight();// 手机屏幕的高度
+        Point size = new Point();
+        windowManager.getDefaultDisplay().getSize(size);
+        int width = size.x;// 手机屏幕的宽度
+        int height = size.y;// 手机屏幕的高度
         int result[] = { width, height };
         return result;
     }
