e997008458d73b4132457ba0bba3e27d1e0657ac
张一鹏 <yipeng.zhang@zymobi.com>
2019-03-14 15:09:55 +0800

    修复手势密码界面的手势事件会穿透到下层View的问题

--- src/org/zywx/wbpalmstar/plugin/uexgestureunlock/fragment/GestureVerifyFragment.java
+++ src/org/zywx/wbpalmstar/plugin/uexgestureunlock/fragment/GestureVerifyFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.zywx.wbpalmstar.base.BDebug;
 import org.zywx.wbpalmstar.base.BUtility;
 import org.zywx.wbpalmstar.engine.universalex.EUExUtil;
 import org.zywx.wbpalmstar.plugin.uexgestureunlock.EUExGestureUnlock.GestureVerifyListener;
@@ -47,6 +49,14 @@ public class GestureVerifyFragment extends GestureBaseFragment implements OnClic
         try {
             view = inflater.inflate(EUExUtil.getResLayoutID("plugin_uexgestureunlock_gesture_verify"),
                     container, false);
+            view.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    // 拦截所有手势事件，防止透传到手势密码界面下面。因为优先级问题，此处不会妨碍手势功能。
+                    BDebug.d("GestureVerifyFragment received onTouch");
+                    return true;
+                }
+            });
             mBg = (RelativeLayout) view.findViewById(EUExUtil.getResIdID("plugin_uexGestureUnlock_bg"));
             mImgUserLogo = (ImageView) view.findViewById(EUExUtil
                     .getResIdID("plugin_uexGestureUnlock_user_logo"));
